d1668604942f7cefed901bbee00bb101586426af
Hugo Baraúna <hugo.barauna@plataformatec.com.br>
2015-12-31 15:15:48 -0200

    Automate vim configs installation

--- install.sh
+++ install.sh
@@ -8,3 +8,13 @@ echo "Installing bin"
 rm -rf ~/bin
 mkdir ~/bin
 ln -s $PWD/bash/functions.sh ~/bin/functions.sh
+
+echo "Installing Vim configs"
+rm ~/.vimrc
+ln -s $PWD/vim/vimrc ~/.vimrc
+
+rm ~/.vimrc.bundles
+ln -s $PWD/vim/vimrc.bundles ~/.vimrc.bundles
+
+rm -f ~/.vim/autoload/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

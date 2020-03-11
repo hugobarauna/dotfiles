511a01515bda5d3d5e29b522d057c3c45f68f9fb
Hugo Baraúna <hugo.barauna@plataformatec.com.br>
2020-03-11 12:43:51 -0300

    Update homebrew bash completion

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -6,9 +6,15 @@ export CLICOLOR="auto"
 
 export EDITOR='vim'
 
-completion="$(brew --prefix)/etc/bash_completion"
-if [ -f $completion ]; then
-  . $completion
+if type brew &>/dev/null; then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+      [[ -r "$COMPLETION" ]] && source "$COMPLETION"
+    done
+  fi
 fi
 
 export GIT_PS1_SHOWDIRTYSTATE=1
